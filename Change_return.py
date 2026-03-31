5f2443e176b898cf0e7ce3d72d96edc496487fd1
Tetra <Tetra3401@gmail.com>
2026-03-31 19:00:14 -0400

    Added file for upload

---
+++ Change_return.py
@@ -0,0 +1,38 @@
+import random
+# Change return program
+# When given a number it will return the change back in dollars and coins
+randomCost = round(random.random() * 200, 2)
+print(f"Your item costs $ {randomCost:.2f}")
+payment = round(float(input("Type out an amount to pay: $")), 2)
+#Convert to cents
+changeTotal = round((payment - randomCost) * 100)
+print(changeTotal)
+
+# Dollars
+dollars = changeTotal // 100
+changeTotal %= 100
+
+# Quarters
+quarters = changeTotal // 25
+changeTotal %= 25
+
+# Dimes
+dimes = changeTotal // 10
+changeTotal %= 10
+
+# Nickels
+nickels = changeTotal // 5
+changeTotal %= 5
+
+# Pennies
+pennies = changeTotal
+
+
+#Print the result
+print()
+print(f"You recieve:")
+print(f"{dollars} dollars")
+print(f"{quarters} quarters")
+print(f"{dimes} dimes")
+print(f"{nickels} nickels")
+print(f"{pennies} pennies.")
